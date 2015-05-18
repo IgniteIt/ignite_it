468dbdfd4898d3ba1c3bf97c976de0a2a1e9c9eb
armi1189 <armi1189@gmail.com>
2015-05-18 13:40:09 +0100

    index has a prompt to add projects

--- ignite/spec/features/projects_feature_spec.rb
+++ ignite/spec/features/projects_feature_spec.rb
@@ -6,5 +6,23 @@ feature 'projects' do
       visit '/'
       expect(page).to have_title('Ignite')
     end
+
+    scenario 'should display a prompt to add a project' do
+      visit '/'
+      expect(page).to have_content 'No projects yet'
+      expect(page).to have_link 'Add a project'
+    end
+  end
+
+  context 'project have been added' do
+    before do
+      Project.create(name: 'Project', description: 'This is the first project')
+    end
+
+    scenario 'display projects' do
+      visit '/'
+      expect(page).to have_content('Project')
+      expect(page).not_to have_content('No projects yet')
+    end
   end
 end
