354da0da97a98eabab55ae51a3e71d213456cee9
paulwallis42 <paulwallis42@gmail.com>
2015-05-19 11:30:38 +0100

    working aws avatar storage

--- ignite/config/environments/development.rb
+++ ignite/config/environments/development.rb
@@ -40,4 +40,13 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
