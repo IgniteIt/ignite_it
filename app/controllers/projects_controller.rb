24a6925f70074ce1e13f2376cb951251451fe2a4
GJMcGowan <gemcgow@gmail.com>
2015-05-20 16:06:27 +0100

    Refactoring and donation leaking fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     @project.set_expiration_date(project_params[:expiration_date])
-    @project.user_id = current_user.id
+    @project.user = current_user
     if @project.save
       redirect_to project_path(@project)
     else
@@ -23,9 +23,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @user = User.find(@project.user_id)
-    @donations = Donation.where(:project_id => @project.id).pluck(:user_id, :amount)
-    # Line below shouldn't work
-    @donation_sum = Donation.sum(:amount, :conditions => {:id => @project.id})
+    @donation_sum = @project.donations.sum(:amount)
   end
 
   def edit
@@ -45,7 +43,7 @@ class ProjectsController < ApplicationController
 
   def destroy
     @project = Project.find(params[:id])
-    if @project.user_id == current_user.id
+    if @project.user == current_user
       @project.destroy
       flash[:notice] = 'Project has been deleted'
       redirect_to '/'
