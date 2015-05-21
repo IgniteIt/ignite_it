5a3f29b1922319973fda6a155f679d9925ad4aa8
Kanishk Walia <kanishk.walia@gmail.com>
2015-05-21 18:02:15 +0100

    Added validation for donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  validates :amount, presence: true
 
   def with_pence(amount)
     amount.to_i * 100
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
+require 'helpers/donations_helper_spec'
 
-RSpec.describe Donation, type: :model do
+describe Donation, :type => :model do
+
+  it { is_expected.to belong_to :user }
+
+  it 'is not valid without an amount' do
+    donation = Donation.new
+    expect(donation).to have(1).error_on(:amount)
+    expect(donation).not_to be_valid
+  end
+
 end
