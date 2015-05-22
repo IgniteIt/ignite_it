b19388f4b4e4a8aa1324ece250c522b4a783d206
paulwallis42 <paulwallis42@gmail.com>
2015-05-22 10:37:16 +0100

    goal not reached email

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,7 +5,7 @@ s = Rufus::Scheduler.singleton
 # Checks to see if a project has been fully funded, past it's expiration date
 # and emails users to tell them to pay up.
 
-s.every '1h' do
+s.every '10s' do
   Rails.logger.info "Checking for completed projects #{Time.now}"
   project_expire_check
 end
@@ -16,11 +16,13 @@ def project_expire_check
     donations = project.donations.sum(:amount)
     if expires < 0 && project.goal <= donations && project.emailed != 'True'
       complete_emailer(project)
+    elsif expires < 0 && project.goal > donations && project.emailed != 'True'
+      goal_not_met(project)
     end
   end
 end
 
-private 
+private
 
 def complete_emailer(project)
   donations = Donation.where(project_id: project.id)
@@ -32,6 +34,21 @@ def complete_emailer(project)
        :to => "#{person.email}",
        :subject => "#{project.name} was a success!",
        :text => "Dear #{person.username}, \n Please pay us. \n Click link to make payment: http://localhost:3000/projects/#{project.id}/charges/new\n Thanks"
+  end
+  Rails.logger.info "Sending Emails on project completion"
+  project.update(emailed: 'True')
+end
+
+def goal_not_met(project)
+  donations = Donation.where(project_id: project.id)
+  donations.each do |donation|
+    person = User.find(donation.user_id)
+    RestClient.post "https://api:key-5367fa0dd4de3f39b6ed08eeb818e4b7"\
+       "@api.mailgun.net/v3/sandboxee3a8623dbd54edbb49b9ee665ebfad2.mailgun.org/messages",
+       :from => "#{project.name} <mailgun@sandboxee3a8623dbd54edbb49b9ee665ebfad2.mailgun.org>",
+       :to => "#{person.email}",
+       :subject => "#{project.name} was a unsuccessful",
+       :text => "Dear #{person.username}, \n As the project failed to reach its funds goal your pledge is no longer required."
   end
   Rails.logger.info "Sending Emails on project completion"
   project.update(emailed: 'True')
