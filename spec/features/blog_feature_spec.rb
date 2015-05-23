ccd2e51d727abd14e8adc744317476b509eff240
GJMcGowan <gemcgow@gmail.com>
2015-05-23 16:13:04 +0100

    Testing blog mailers

--- spec/features/blog_feature_spec.rb
+++ spec/features/blog_feature_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 require 'helpers/projects_helper_spec'
 require 'helpers/users_helper_spec'
+require 'helpers/blogs_helper_spec'
+require 'helpers/donations_helper_spec'
 
 feature 'blogs' do
   include ProjectsHelper
   include UserHelper
+  include DonationsHelper
+  include BlogsHelper
 
   before do
     sign_up
     create_project('Campaign', regular_description, '100', '30 days from now', 'Environment', 'London')
     configure_project_stubs
+    configure_blog_stubs
   end
 
   context 'Projects and their blogs' do
     scenario 'Owner creates a blog' do
-      click_link 'Campaign'
-      click_link 'Create Blog'
-      fill_in 'Title', with: 'Blog 1'
-      fill_in 'Content', with: 'This is my project'
-      click_button 'Submit'
+      make_blog
       expect(page).to have_content('Blog 1')
       expect(page).to have_content('This is my project')
       expect(current_path).to eq ("/projects/#{Project.last.id}")
@@ -32,7 +33,12 @@ feature 'blogs' do
       expect(page).to have_content('You are not the project owner')
     end
 
-    xscenario 'When owner makes blog, all donors are notified' do
+    scenario 'When owner makes a blog, all donors are notified' do
+      make_payment(25)
+      make_blog
+      expect(WebMock).to have_requested(:post, 
+        "https://api:#{ENV['MAILGUN_KEY']}@api.mailgun.net/v3/sandboxee3a8623dbd54edbb49b9ee665ebfad2.mailgun.org/messages"
+        ).with(:body => hash_including({"from"=>"Campaign <mailgun@sandboxee3a8623dbd54edbb49b9ee665ebfad2.mailgun.org>", "subject"=>"Campaign has posted a blog"}))
     end
   end
 end
