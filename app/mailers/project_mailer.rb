7a60274fc94755a256a7e083a52e3f41783d16ac
GJMcGowan <gemcgow@gmail.com>
2015-05-26 19:22:37 +0100

    Adding password reset email

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -11,4 +11,14 @@ class ProjectMailer < ApplicationMailer
          Rails.logger.info "I have been called. The user is #{person.username}"
     end
   end
+
+  def reset_password_instructions(person, token, opts={})
+    RestClient.post "https://api:#{ENV['MAILGUN_KEY']}"\
+         "@api.mailgun.net/v3/sandboxee3a8623dbd54edbb49b9ee665ebfad2.mailgun.org/messages",
+         :from => "#IgniteIt <mailgun@sandboxee3a8623dbd54edbb49b9ee665ebfad2.mailgun.org>",
+         :to => "#{person.email}",
+         :subject => "Reset your password",
+         :text => "Dear #{person.username}, \n Please reset your password: http://localhost:3000/users/password/edit?reset_password_token=#{token}\n IgniteIt"
+         Rails.logger.info "I have been called. The user is #{person.username}"
+  end
 end
