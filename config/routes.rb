28a23b011b3e4a2f680d9da02e41adcf1d609c76
GJMcGowan <gemcgow@gmail.com>
2015-05-28 18:03:50 +0100

    Cleaning up forms a bit

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,4 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
-  # devise_scope :user do
-  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
 end
