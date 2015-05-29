aa11cca93aef062493a395ec42e38da3ecaf7fba
armi1189 <armi1189@gmail.com>
2015-05-29 11:41:05 +0100

    landing page done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( close.png )
 Rails.application.config.assets.precompile += %w( spinner.gif )
+Rails.application.config.assets.precompile += %w( home.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
